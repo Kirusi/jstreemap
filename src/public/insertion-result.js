07d60230d064b169e65758b58c395687882ac890
Kirusi Msafiri <kirusi.msafiri@gmail.com>
2018-01-11 11:34:26 -0500

    Added missing documentation in InsertionResult

--- src/public/insertion-result.js
+++ src/public/insertion-result.js
@@ -3,6 +3,12 @@
  * if a node was added, or an existing one replaced then an iterator is provided. Otherwise the value of iterator is undefined
  */
 class InsertionResult {
+    /**
+     * Default constructor
+     * @param {Boolean} wasAdded 
+     * @param {Boolean} wasReplaced 
+     * @param {Iterator} iterator only provided if the node was added, or replaced
+     */
     constructor(wasAdded, wasReplaced, iterator) {
         /**
          * Boolean flag indicating whether an element was added
